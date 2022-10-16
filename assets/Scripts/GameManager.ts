7d505b13b2e2e3533bddcffcb206bb4b110eb679
zhang <877289706@qq.com>
2022-10-16 15:15:49 +0800

    first commit

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,175 @@
+import { _decorator, Component, Node, Prefab, instantiate, Vec3, Label } from 'cc';
+import { PlayerController } from './PlayerController';
+const { ccclass, property } = _decorator;
+
+//赛道各自类型，坑（BT_BONE）或者实路（BT_STONE）
+enum BlockType {
+    BT_NONE,
+    BT_STONE,
+};
+
+enum GameState {
+    GS_INIT,
+    GS_PLAYING,
+    GS_END,
+};
+
+@ccclass('GameManager')
+export class GameManager extends Component {
+
+    //赛道预制
+    @property({type : Prefab})
+    public cubePrfb: Prefab | null = null;
+
+    @property({type : PlayerController})
+    public playerCtl: PlayerController | null = null;
+
+    @property({type : Node})
+    public startMenu: Node | null = null;
+
+    @property
+    public roadLength = 50;
+
+    @property({type: Label})
+    public stepsLabel: Label | null = null;
+
+    private _road: BlockType[] = [];
+
+    start() {
+        // this.generateRoad();
+        this.curState = GameState.GS_INIT;
+        this.playerCtl?.node.on('JumpEnd', this.onPlayerJumpEnd, this);
+    }
+
+    // update(deltaTime: number) {}
+
+
+    /**
+     * 初始化方法
+     */
+    init() {
+        // 激活主页面
+        if (this.startMenu) {
+            this.startMenu.active = true;
+        }
+
+        // 生成跑道
+        this.generateRoad();
+        if (this.playerCtl) {
+            this.playerCtl.setInputActive(false);
+            // 重制人物位置
+            this.playerCtl.node.setPosition(Vec3.ZERO);
+            //重制步数
+            this.playerCtl.reset();
+        }
+    }
+
+    set curState (value: GameState) {
+        switch(value) {
+            case GameState.GS_INIT:
+                this.init();
+                break;
+            case GameState.GS_PLAYING:
+                if (this.startMenu) {
+                    this.startMenu.active =false;
+                }
+
+                if(this.stepsLabel){
+                    this.stepsLabel.string = '0';
+                }
+
+                // 设置 active 为 true 时会直接开始监听鼠标事件，此时鼠标抬起事件还未派发
+                // 会出现的现象就是，游戏开始的瞬间人物已经开始移动
+                // 因此，这里需要做延迟处理
+                setTimeout(() => {
+                    if (this.playerCtl) {
+                        this.playerCtl.setInputActive(true);
+                    }
+                }, 0.1);
+                break;
+            case GameState.GS_END:
+                break;
+                
+        }
+    }
+
+    /**
+     * 监听游戏开始
+     */
+    onStartButtonClicked() {
+        this.curState = GameState.GS_PLAYING;
+    }
+
+    /**
+     * 生成赛道
+     */
+    generateRoad() {
+        // 防止游戏重新开始时，赛道还是旧的赛道
+        // 因此，需要移除旧赛道，清除旧赛道数据
+        this.node.removeAllChildren();
+        this._road = [];
+
+        // 确保游戏运行时，人物一定站在实路上
+        this._road.push(BlockType.BT_STONE);
+
+        // 确定好每一格赛道类型
+        for (let i = 1; i < this.roadLength; i++) {
+            // 如果上一格赛道是坑，那么这一格一定不能为坑
+            if (this._road[i-1] === BlockType.BT_NONE) {
+                this._road.push(BlockType.BT_STONE);
+            } else {
+                this._road.push(Math.floor(Math.random() * 2));
+            }
+        }
+
+        // 根据赛道类型生成赛道
+        for (let j = 0; j < this.roadLength; j++) {
+            let block: Node = this.spawnBlockByType(this._road[j]);
+            if (block) {
+                this.node.addChild(block);
+                block.setPosition(j, -1.5, 0);
+            }
+        }
+    }
+
+    /**
+     * 实例化赛道的砖
+     * 
+     * @param type 
+     * @returns 
+     */
+    spawnBlockByType (type : BlockType) {
+        if (!this.cubePrfb){
+            return null;
+        }
+
+        let block: Node | null = null;
+        // 赛道类型为实路才生成
+        switch(type) {
+            case BlockType.BT_STONE:
+                block = instantiate(this.cubePrfb);
+                break;
+        }
+
+        return block;
+    }
+    
+    checkResult(moveIndex: number) {
+        if (moveIndex < this.roadLength){
+            // 跳到坑上
+            if (this._road[moveIndex] == BlockType.BT_NONE) {
+                this.curState = GameState.GS_INIT;
+            }
+        } else {
+            this.curState = GameState.GS_INIT;
+        }
+    }
+
+    onPlayerJumpEnd(moveIndex: number) {
+        if (this.stepsLabel){
+            this.stepsLabel.string = '' + (moveIndex >= this.roadLength ? this.roadLength : moveIndex);
+        }
+        this.checkResult(moveIndex);
+    }
+}
+
